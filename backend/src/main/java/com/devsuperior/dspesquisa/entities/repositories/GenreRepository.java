548f0ef621194252df82ea585c5847c8f38b60ea
Leonardo Vieira <Leonardovieira54@hotmail.com>
2020-09-18 11:59:43 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/repositories/GenreRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
-package com.devsuperior.dspesquisa.repositories;
+package com.devsuperior.dspesquisa.entities.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dspesquisa.entities.Genre;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-
+//este foi criado hj 18/-9
 }
